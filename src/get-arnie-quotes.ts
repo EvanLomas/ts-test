aa5a9ed88082ba10c643e22ed215b5ccf5318fcc
Zeus3rd <zeus3rd@mgail.com>
2022-02-21 16:08:51 +1100

    getArnieQuotes() function structure added

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,39 @@
 import { httpGet } from './mock-http-interface';
 
-// TODO define this type properly
-type TResult = any;
+const MESSAGE_ERROR = 'Your request has been terminated';
+
+type TResult = {
+  'Arnie Quote'?: String,
+  'FAILURE'?: typeof MESSAGE_ERROR,
+};
 
 export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  // TODO: Implement this function.
-  
-  return [];
-};
+  const requests = await urls.map(async (url) => {
+    var results : TResult
+
+    // Response Check
+    try {
+      const {status,body} = await httpGet(url);
+      
+      switch (status) {
+        // Success
+        case 200:
+          results = { 'Arnie Quote': JSON.parse(body).message }
+          break;
+      
+        // Failure/Other
+        default:
+          results = { 'FAILURE': MESSAGE_ERROR };
+          break;
+      }
+
+    } catch (e) {
+      // Catch Unexpected Responses
+      results = { 'FAILURE': MESSAGE_ERROR };
+    }
+
+    return results;
+  })
+
+  return Promise.all(requests)
+}
