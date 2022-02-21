371df6491efd859316252e0de0be173a3ad5a994
Zeus3rd <zeus3rd@mgail.com>
2022-02-21 17:44:06 +1100

    Correction to error response text and removed try/catch error wrapper in getArnieQuotes() for simplicity

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,8 @@
 import { httpGet } from './mock-http-interface';
 
-const MESSAGE_ERROR = 'Your request has been terminated';
-
 type TResult = {
   'Arnie Quote'?: String,
-  'FAILURE'?: typeof MESSAGE_ERROR,
+  'FAILURE'?: String,
 };
 
 export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
@@ -12,24 +10,18 @@ export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
     var results : TResult
 
     // Response Check
-    try {
-      const {status,body} = await httpGet(url);
-      
-      switch (status) {
-        // Success
-        case 200:
-          results = { 'Arnie Quote': JSON.parse(body).message }
-          break;
-      
-        // Failure/Other
-        default:
-          results = { 'FAILURE': MESSAGE_ERROR };
-          break;
-      }
-
-    } catch (e) {
-      // Catch Unexpected Responses
-      results = { 'FAILURE': MESSAGE_ERROR };
+    const {status,body} = await httpGet(url);
+    
+    switch (status) {
+      // Success
+      case 200:
+        results = { 'Arnie Quote': JSON.parse(body).message }
+        break;
+    
+      // Failure/Other
+      default:
+        results = { 'FAILURE': JSON.parse(body).message };
+        break;
     }
 
     return results;
